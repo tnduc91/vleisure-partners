2995c94cb856ea9ca0175fe3e21929e3e291ef72
Nam Nguyen Thanh <namnt3108@gmail.com>
2018-11-04 22:58:25 +0700

    Add Unity

---
+++ VleisurePartner.Web/App_Start/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Net;
+
+[assembly: OwinStartup(typeof(VleisurePartner.Web.App_Start.Startup))]
+namespace VleisurePartner.Web.App_Start
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+            //ConfigureAuth(app);
+        }
+    }
+}
---
+++ VleisurePartner.Web/App_Start/UnityConfig.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Linq;
+using Unity;
+using Unity.RegistrationByConvention;
+using VleisurePartner.Web.App_Start;
+
+namespace VleisurePartner.Web.App_Start
+{
+    /// <summary>
+    /// Specifies the Unity configuration for the main container.
+    /// </summary>
+    public static class UnityConfig
+    {
+        private static string _assemblyVersion;
+        private static string _assemblyDate;
+
+        /// <summary>
+        /// Gets the configured Unity container.
+        /// </summary>
+        public static IUnityContainer GetConfiguredContainer()
+        {
+            return Container.Value;
+        }
+
+        private static readonly Lazy<IUnityContainer> Container = new Lazy<IUnityContainer>(() =>
+        {
+            var assembly = System.Reflection.Assembly.GetAssembly(typeof(Startup));
+            _assemblyVersion = assembly.GetName().Version.ToString();
+            _assemblyDate = System.IO.File.GetLastWriteTime(assembly.Location)
+                .ToString(System.Globalization.CultureInfo.CurrentCulture);
+
+            var container = new UnityContainer();
+            RegisterTypes(container);
+            AutoRegisterInterfaces(container);
+            return container;
+        });
+
+        private static void RegisterTypes(IUnityContainer container)
+        {
+        }
+
+
+        private static void AutoRegisterInterfaces(IUnityContainer container)
+        {
+            var allowedRootNamespaces = new[] {"VleisurePartner", ""};
+            var excludedRootNamespaces = new[] {"VleisurePartner.EF", "VleisurePartner.Domain"};
+
+            var interfacesWithOneImplementation = AllClasses.FromLoadedAssemblies()
+                .Where(TypeNamespacesStartWith(allowedRootNamespaces))
+                .Where(TypeNamespacesNotStartWith(excludedRootNamespaces))
+                .SelectMany(t =>
+                    t.GetInterfaces()
+                        .Select(i => new
+                        {
+                            ClassType = t,
+                            Interface = t.IsGenericType && i.IsGenericType
+                                ? i.GetGenericTypeDefinition()
+                                : i
+                        }))
+                .Where(x => !container.IsRegistered(x.Interface))
+                .GroupBy(x => x.Interface)
+                .Where(x => x.Count() == 1)
+                .SelectMany(x => x);
+
+            foreach (var registerTypePair in interfacesWithOneImplementation)
+            {
+                container.RegisterType(registerTypePair.Interface, registerTypePair.ClassType);
+            }
+        }
+
+        private static Func<Type, bool> TypeNamespacesStartWith(string[] allowedRootNamespaces)
+        {
+            return t => allowedRootNamespaces.Any(allowedNamespace =>
+                t.Namespace != null
+                && t.Namespace.StartsWith(allowedNamespace, StringComparison.OrdinalIgnoreCase));
+        }
+
+        private static Func<Type, bool> TypeNamespacesNotStartWith(string[] excludedNamespaces)
+        {
+            return t => excludedNamespaces.All(excludedNamespace =>
+                t.Namespace != null
+                && !t.Namespace.StartsWith(excludedNamespace, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
