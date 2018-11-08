409ae5e3cff25cfb20687304b10a93238cea8ef3
Nam Nguyen Thanh <namnt3108@gmail.com>
2018-11-08 23:58:31 +0700

    More code, more fun.

--- VleisurePartner.Domain/IContext.cs
+++ VleisurePartner.Domain/IContext.cs
@@ -2,5 +2,10 @@
 {
     public interface IContext
     {
+        //IDbSet<Accpac> Accpacs { get; set; } // Accpac
+
+        int SaveChanges();
+        void SetCommandTimeout(int timeout);
+        void SetRowStamp(object entity, byte[] rowStamp);
     }
 }
