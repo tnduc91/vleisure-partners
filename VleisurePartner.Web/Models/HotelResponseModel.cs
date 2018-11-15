e6671da24f7c856b753136e31ae39b42feeda64c
Duc <tnduc91@gmail.com>
2018-11-15 16:35:03 +0700

    Inherit ITypeProxy for automatically generating *.ts file

--- VleisurePartner.Web/Models/HotelResponseModel.cs
+++ VleisurePartner.Web/Models/HotelResponseModel.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using VleisurePartner.Web.Infrastructure.Typescript;
 
 namespace VleisurePartner.Web.Models
 {
-    public class HotelResponseModel
+    public class HotelResponseModel : ITypeProxy
     {
         public string HotelId { get; set; }
         public string Name { get; set; }
---
+++ VleisurePartner.Web/src/proxy/HotelResponseModel.ts
@@ -0,0 +1,34 @@
+﻿
+/* tslint:disable */
+declare namespace ProxyModel{
+    
+    export interface HotelResponseModel  {
+        
+        hotelId: string;
+        name: string;
+        address1: string;
+        address2: string;
+        city: string;
+        stateProvinceCode: string;
+        countryCode: string;
+        airportCode: string;
+        cityCode: string;
+        sessionId: string;
+        propertyCategory: string;
+        hotelRating: string;
+        hotelRatingDisplay: string;
+        confidenceRating: string;
+        amenityMask: string;
+        locationDescription: string;
+        shortDescription: string;
+        highRate: string;
+        lowRate: string;
+        latitude: string;
+        longitude: string;
+        proximityDistance: string;
+        proximityUnit: string;
+        hotelInDestination: string;
+        thumbNailUrl: string;
+        }
+    
+}
